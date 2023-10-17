aeb83d2f8d1bf8a9621f6328e7c3cd51ecd4aaa9
dev-deepak-soni <deepaksoni@amwebtech.com>
2023-10-17 14:27:59 +0530

    feat(Replace js variable)

---
+++ variable.js
@@ -0,0 +1,48 @@
+// Variable declaration with 'var'
+var name = "John";
+var age1 = 30;
+var isStudent = false;
+
+// Variable declaration with 'let' (introduced in ES6)
+let city = "New York";
+let population = 8175133;
+
+// Variable declaration with 'const' (constant)
+const PI = 3.14;
+const MAX_USERS = 100;
+
+// Arrays
+let fruits = ["apple", "banana", "cherry"];
+let numbers = [1, 2, 3, 4, 5];
+
+// Objects
+let person = {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 25,
+    isStudent: true
+};
+
+// Function
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// Date object
+let today = new Date();
+
+// Regular expression
+let pattern = /[a-zA-Z]+/;
+
+// Null and undefined
+let nullValue = null;
+let undefinedValue;
+
+// Symbols (introduced in ES6)
+let uniqueSymbol = Symbol("description");
+
+// Template literals (introduced in ES6)
+let message = `Hello, ${name}!`;
+
+// BigInt (introduced in ES11)
+let bigIntValue = 1234567890123456789012345678901234567890n;
